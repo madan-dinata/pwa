7b7a2b4b020b7e1190f984eda9fc938539c5a86b
madan-dinata <madanmr48@gmail.com>
2020-01-23 21:05:07 +0700

    menambahkan event listener untuk klik menu

--- js/nav.js
+++ js/nav.js
@@ -34,25 +34,25 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 	// Load page content
-	let page = window.location.hast.substr(1)
-	if (page == "") page = "home"
-		loadPage(page)
-
+	let page = window.location.hash.substr(1);
+	if (page == "") page = "home";
+	loadPage(page);
+	 
 	function loadPage(page) {
-		let xhttp = new XMLHttpRequest()
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4) {
-				let content = document.querySelector("#body-content")
-				if (this.status == 200) {
-					content.innerHTML = xhttp.responseText
-				} else if (this.status == 404) {
-					content.innerHTML = "<p>Halaman tidak ditemukan.</p>"
-				} else {
-					content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>"
-				}
-			}
-		}
-		xhttp.open("GET", "pages/" + page + ".html", true)	
-		xhttp.send()
+	  let xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4) {
+	      let content = document.querySelector("#body-content");
+	      if (this.status == 200) {
+	        content.innerHTML = xhttp.responseText;
+	      } else if (this.status == 404) {
+	        content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+	      } else {
+	        content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+	      }
+	    }
+	  };
+	  xhttp.open("GET", "pages/" + page + ".html", true);
+	  xhttp.send();
 	}
 })
