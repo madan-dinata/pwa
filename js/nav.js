b542d2941feecda48defea01c7c680f9405aab22
madan-dinata <madanmr48@gmail.com>
2020-01-23 20:52:31 +0700

    menambahkan event listener untuk klik menu

--- js/nav.js
+++ js/nav.js
@@ -14,6 +14,19 @@ document.addEventListener("DOMContentLoaded", function() {
 				document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
 					elm.innerHTML = xhttp.responseText
 				})
+
+				// Daftarkan event listener untuk setiap tautan menu
+				document.querySelectorAll(".sidenav a, .topnav a").forEach(function(elm) {
+					elm.addEventListener("click", function(event) {
+						// Tutup sidenav
+						let sidenav = document.querySelector(".sidenav")
+						M.Sidenav.getInstance(sidenav).close()
+
+						// Muat konten halaman yang di panggil
+						page = event.target.getAttribute("href").substr(1)
+						loadPage(page)
+					})
+				})
 			}
 		}
 		xhttp.open("GET", "nav.html", true)
