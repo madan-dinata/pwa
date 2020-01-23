b6a96b6fc3d9a0762bd08eba6aa1f061ce54023c
madan-dinata <madanmr48@gmail.com>
2020-01-23 20:44:07 +0700

    menampilkan konten halaman

--- js/nav.js
+++ js/nav.js
@@ -19,4 +19,27 @@ document.addEventListener("DOMContentLoaded", function() {
 		xhttp.open("GET", "nav.html", true)
 		xhttp.send()
 	}
+
+	// Load page content
+	let page = window.location.hast.substr(1)
+	if (page == "") page = "home"
+		loadPage(page)
+
+	function loadPage(page) {
+		let xhttp = new XMLHttpRequest()
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				let content = document.querySelector("#body-content")
+				if (this.status == 200) {
+					content.innerHTML = xhttp.responseText
+				} else if (this.status == 404) {
+					content.innerHTML = "<p>Halaman tidak ditemukan.</p>"
+				} else {
+					content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>"
+				}
+			}
+		}
+		xhttp.open("GET", "pages/" + page + ".html", true)	
+		xhttp.send()
+	}
 })
